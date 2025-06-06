9f23d3dc991f1e214cb313c142e2b4fc47ff09ae
shubraj <shubham.a.raj@capgemini.com>
2025-06-06 12:51:07 +0530

    test1

---
+++ src/EX_1_22/SumThreeNo.java
@@ -0,0 +1,40 @@
+package EX_1_22;
+
+import java.util.Scanner;
+
+public class SumThreeNo {
+    public static void main(String[] args){
+        //Question 1 - Sum of three no
+        /*int a = 12;
+        int b = 10;
+        int c = 15;
+
+        int sum = a+b+c;
+        System.out.println(sum); */
+
+        //
+        // Question2 - Cgpa
+      /*  float Math = 45 ;
+        float Scince = 80;
+        float SST = 60;
+        float Avg = (Math + Scince+SST)/3;
+        float cgpa = (Avg/10);
+        System.out.println(cgpa +"   Cgpa obtained by the shubham");
+
+       */
+
+       // Question to ask neame from user and print that
+      /*  System.out.println("What is your name ");
+        Scanner sc = new Scanner(System.in);
+        String name  = sc.next();
+        System.out.println("hello " +name+" have a good day ");
+
+
+       */
+     System.out.println("From where are you ");
+     Scanner sc = new Scanner(System.in);
+     String name = sc.next();
+     System.out.println("hello I am from  " + name + "   Gaya ");
+
+    }
+}
